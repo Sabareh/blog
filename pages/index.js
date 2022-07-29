06361a79eee488d607e3a8d80fae7f564cdacee7
Sabare Victor Oketch <40304396+Sabareh@users.noreply.github.com>
2022-07-29 18:10:45 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -26,11 +26,11 @@ export default function Home({ posts }) {
           <div className="pt-6">
             <h1 className="pb-6 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
               Hi, I am{' '}
-              <span className="text-primary-color-500 dark:text-primary-color-dark-500">
+              <span className="text-primary-color-800 dark:text-primary-color-dark-500">
                 Victor Sabare
               </span>
             </h1>
-            <h2 className="prose pt-5 text-lg text-gray-600 dark:text-gray-300">
+            <h2 className="prose pt-5 text-lg text-gray-600 dark:text-gray-50">
               {`Welcome to ${siteMetadata.description}. I am a Data apprentice who is passionate about Data Science, Machine Learning and Artificial Intelligence. In my free time, I like developing `}
               side projects and learning new technologies.
             </h2>
@@ -42,10 +42,10 @@ export default function Home({ posts }) {
                 show={true}
                 color="#DE1D8D"
                 animationDelay={1000}
-                animationDuration={2500}
+                animationDuration={3000}
                 className="text-slate-200"
               >
-                thoughts, reflections & everything&nbsp;
+                thoughts, reflections & everything &nbsp;
               </RoughNotation>
               in between. Have a good read!
             </p>
