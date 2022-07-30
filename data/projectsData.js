b13d2c02bf570a69fb86deaa0f0ef5c7c6b0c385
Sabare Victor Oketch <40304396+Sabareh@users.noreply.github.com>
2022-07-30 03:56:08 +0300

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -2,7 +2,7 @@ const projectsData = [
   {
     title: 'Python Flask AI translation service',
     description: `This is a web app made using Python-Flask framework that integrates the AI cognitive service of Azure.`,
-    imgSrc: '',
+    imgSrc: '/public/static/images/Blog/wwdc-2022-hero.jpeg',
     href: 'https://python-ai-project.azurewebsites.net',
     //github: 'https://github.com/pycoder2000/Predicting-Number-of-Shares',
     tech1: 'Azure',
