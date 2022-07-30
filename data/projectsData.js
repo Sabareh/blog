dc155bbccce12671b2095885e558851319c689ad
Sabare Victor Oketch <40304396+Sabareh@users.noreply.github.com>
2022-07-30 03:16:54 +0300

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,16 +1,6 @@
 const projectsData = [
   {
-    title: 'InstantMD',
-    description: `Instant MD is an Investigation, Medication and Chief complaint recognition application using Natural Language Processing. 🏆 Winner of the HealthCare Track at the MINeD Hackathon.`,
-    imgSrc: '',
-    href: '',
-    github: 'https://github.com/pycoder2000/InstantMD',
-    tech1: 'Regex',
-    tech2: 'Flask',
-    tech3: 'Semantic UI',
-  },
-  {
-    title: 'Car Price Predictor',
+    title: 'Car Price Predictor'aaa,
     description: `The Car Price Predictor is a web application which predicts car prices based on given independent features like Car Name, Year, Selling Price, Present Price, Kms Driven, Fuel Type etc. It is made using Flask and deployed on Heroku.`,
     imgSrc: '',
     href: 'https://allysoncaragency.herokuapp.com/',
