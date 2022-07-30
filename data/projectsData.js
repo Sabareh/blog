74fb82919c54063851de34ef7a0657accad0590c
Sabare Victor Oketch <40304396+Sabareh@users.noreply.github.com>
2022-07-30 03:20:24 +0300

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,16 +1,5 @@
 const projectsData = [
   {
-    title: 'Salary Predictor',
-    description: `Salary Prediction App made with StreamLit just to practice the Streamlit framework. A Streamlit demo written in pure Python to predict salary based on your years of experience.`,
-    imgSrc: '',
-    href: 'https://salary-predictor-stream.herokuapp.com/',
-    github: 'https://github.com/pycoder2000/Salary',
-    tech1: 'Python',
-    tech2: 'Streamlit',
-    tech3: 'Javascript',
-  },
-  
-  {
     title: 'Python Flask AI translation service',
     description: `This is a web app made using Python-Flask framework that integrates the AI cognitive service of Azure.`,
     imgSrc: '',
