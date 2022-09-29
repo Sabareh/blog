57c99d6c53de5f6d8bfdbc3810d33fd735c7a9d7
Victor Oketch Sabare <sabarevictor@gmail.com>
2022-09-29 23:09:14 +0300

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -60,7 +60,6 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
             <ProgressBar bgcolor="#DE1D8D" />
             <ScrollTop />
             <Head>
-              <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4331473606799485" crossorigin="anonymous"></script>
               <meta content="width=device-width, initial-scale=1" name="viewport" />
             </Head>
             {isDevelopment && isSocket && <ClientReload />}
