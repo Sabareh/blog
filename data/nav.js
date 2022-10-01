06b21d51975f00d069da508ec1b1214e0c1d8968
Victor Oketch Sabare <sabarevictor@gmail.com>
2022-10-01 13:27:04 +0300

    Update nav.js

--- data/nav.js
+++ data/nav.js
@@ -17,6 +17,7 @@ export const navigation = {
     { name: 'Quotes', href: '/quotes' },
     { name: 'Tweets', href: '/tweets' },
     { name: 'RSS', href: '/feed.xml' },
+    {name: 'ads.txt', href: '/ads.txt'},
     // { name: 'Guestbook', href: '/guestbook' },
     // { name: 'Bookmarks', href: '/bookmarks' },
     // { name: 'Todo', href: '/todo' },
