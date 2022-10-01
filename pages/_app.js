2ddd2db42b094c0af2791f422acb372fec278253
Victor Oketch Sabare <sabarevictor@gmail.com>
2022-10-01 18:27:54 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -76,3 +76,30 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
     </SessionProvider>
   )
 }
+export function GoogleAdsenseContainer ( { client, slot }) {
+
+  useEffect(() => {
+    (window.adsbygoogle = window.adsbygoogle || []).push({});
+  }, []);
+
+  const AdLabel = styled.span`
+    font-size: 12px;
+  `
+
+  return (
+    <div 
+      style={{textAlign: 'left',overflow: 'hidden'}}
+    >
+    <AdLabel>Advertisment</AdLabel>
+      <ins
+        className="adsbygoogle"
+        style={{ display: "block" }}
+        data-ad-client={client}
+        data-ad-slot={slot}
+        data-ad-format="auto"
+        data-full-width-responsive="true"
+      ></ins>
+
+    </div>
+  );     
+}
