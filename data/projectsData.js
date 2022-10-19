9f428fa333e60c96c9470abcedec39efa76836aa
Victor Oketch Sabare <sabarevictor@gmail.com>
2022-10-19 13:42:35 +0300

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -9,6 +9,17 @@ const projectsData = [
     tech2: 'Machine Learning',
     tech3: 'Linear Regression',
   },
+  {
+    title: 'Data-Engineering-Project-1.0-using-Sales-Data',
+    description: `Data Engineering in Hadoop using Cloudera Performed the principle tasks involved in managing, loading, extracting, and transforming data. This respository holds the scripst that I wrote during the whole project. The project was done in Cloudera using Hadoop.`,
+    imgSrc: '/static/images/Blog/wwdc-2022-hero.jpeg',
+    href: 'https://github.com/Sabareh/Data-Engineering-Project-1.0-using-Sales-Data',
+    //github: 'https://github.com/pycoder2000/Predicting-Number-of-Shares',
+    tech1: 'Cloudera',
+    tech2: 'Hadoop',
+    tech3: 'SQL',
+    tech4: 'Data Engineering',
+  },
 ]
 
 export default projectsData
