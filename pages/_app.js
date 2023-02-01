21716d1be4649744c5d5f13b51e76df4ab5f44e8
Victor Oketch Sabare <sabarevictor@gmail.com>
2023-02-01 18:00:08 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -63,7 +63,6 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
               data-ad-client="ca-pub-4331473606799485"
               async
               src="/scripts/google_script.js"
-              crossOrigin="anonymous"
             ></script>
             <meta content="width=device-width, initial-scale=1" name="viewport" />
           </Head>
