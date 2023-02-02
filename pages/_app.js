5c45935902aedd9afa686c8a4891f06fe396452e
Victor Oketch Sabare <sabarevictor@gmail.com>
2023-02-02 11:56:48 +0300

    trying out index

--- pages/_app.js
+++ pages/_app.js
@@ -59,11 +59,6 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
           <ProgressBar bgcolor="#DE1D8D" />
           <ScrollTop />
           <Head>
-            <Script
-              data-ad-client="ca-pub-4331473606799485"
-              async
-              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-            ><Script/>
             <meta content="width=device-width, initial-scale=1" name="viewport" />
           </Head>
           {isDevelopment && isSocket && <ClientReload />}
