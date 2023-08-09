71267312ddf88af339f7b9007096e379c132dad9
Victor Oketch Sabare <sabarevictor@gmail.com>
2023-08-09 10:25:20 +0300

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -46,6 +46,15 @@ const projectsData = [
     tech4: 'Hive',
     tech5: 'Data Engineering',
   },
+  {
+    title: 'A visual history of Nobel Prize winners',
+    description: `The Nobel Prize is perhaps the world's most well-known scientific award. Except for the honor, prestige, and substantial prize money, the recipient also gets a gold medal showing Alfred Nobel (1833 - 1896), who established the prize. Every year it's given to scientists and scholars in chemistry, literature, physics, physiology or medicine, economics, and peace. The first Nobel Prize was handed out in 1901, and at that time, the Prize was very Eurocentric and male-focused, but nowadays it's not biased in any way whatsoever. Surely. Right?`,
+    href: 'https://github.com/Sabareh/A-Visual-History-of-Nobel-Prize-Winners',
+    tech1: 'Python',
+    tech2: 'Data Science',
+    tech3: 'Data Visualization',
+  },
+
 ] 
 
 export default projectsData
