bde2338c939fd0d902ded0cba9e0b6cb940f3149
Victor Oketch Sabare <sabarevictor@gmail.com>
2023-08-09 09:41:58 +0300

    Update projectsData.js

--- data/projectsData.js
+++ data/projectsData.js
@@ -4,22 +4,39 @@ const projectsData = [
     description: `This is a web app made using Python-Flask framework that integrates the AI cognitive service of Azure.`,
     imgSrc: '/static/images/Blog/wwdc-2022-hero.jpeg',
     href: 'https://python-ai-project.azurewebsites.net',
-    //github: 'https://github.com/pycoder2000/Predicting-Number-of-Shares',
     tech1: 'Azure',
     tech2: 'Machine Learning',
     tech3: 'Linear Regression',
   },
   {
-    title: 'Data-Engineering-Project-1.0-using-Sales-Data',
+    title: 'Data-Engineering-Project-using-Sales-Data',
     description: `Data Engineering in Hadoop using Cloudera. Performed the principle tasks involved in managing, loading, extracting, and transforming data. This project respository holds the scripst that I wrote during the whole project. The project was done in Cloudera using Hadoop.`,
     imgSrc: '/static/images/Blog/wwdc-2022-hero.jpeg',
     href: 'https://github.com/Sabareh/Data-Engineering-Project-1.0-using-Sales-Data',
-    //github: 'https://github.com/pycoder2000/Predicting-Number-of-Shares',
     tech1: 'Cloudera',
     tech2: 'Hadoop',
     tech3: 'SQL',
     tech4: 'Data Engineering',
   },
+  {
+    title: 'Stock price prediction with Apache spark and cassandra',
+    description: `This is a data pipeline for predicting stock prices using Apache Spark, Apache Cassandra, and machine learning techniques. It collects and preprocesses stock data from Alpha Vantage API, engineers features, trains models, and performs data analysis and predictions.`,
+    imgSrc: '/static/images/Blog/wwdc-2022-hero.jpeg',
+    href: 'https://github.com/Sabareh/stock-price-prediction-spark-cassandra',
+    tech1: 'Apache Spark',
+    tech2: 'Apache Cassandra',
+    tech3: 'Machine Learning',
+    tech4: 'Data Engineering',
+  },
+  {
+    title: 'The GitHub History of the Scala language',
+    description: `Find out who has had the most influence on its development and who are the experts. Explore the evolution of the Scala language through its vibrant GitHub history. This is a comprehensive collection of historical data, commits, issues, and pull requests related to the development of Scala, a modern, multi-paradigm programming language.',
+    imgSrc: '/static/images/Blog/wwdc-2022-hero.jpeg', 
+    href: 'https://github.com/Sabareh/The-GitHub-History-of-the-Scala-language,
+    tech1: 'Scala',
+    tech2: 'GitHub',
+    tech3: 'Data Science',
+',
 ]
 
-export default projectsData
+export default projectsData 
