9762cf91fb152f22aef4e0fba73141aeae97a9aa
Victor Oketch Sabare <sabarevictor@gmail.com>
2024-05-27 16:32:51 +0300

    Update experienceData.js

--- data/experienceData.js
+++ data/experienceData.js
@@ -9,7 +9,7 @@ const experienceData = [
     text2: 'Written 1500-word sections for 100+ landing pages, covering topics from technical interview questions to FAQs about the hiring process.',
     text3: 'Writing articles on the “employer blog” for founders and business owners looking to outsource talent in Latin America.',
     text4: 'Creating engaging and informative content that helps clients find the best talent for their projects.',
-    text5: 'Increased the overall SEO ranking of the company's website on Google by 16%.',
+    text5: 'Increased the overall SEO ranking of the company website on Google by 16%.',
   },
 ]
 
